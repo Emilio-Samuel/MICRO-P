fd8ee25706b30e9540222f14674d9227ef27966e
Emilio Samuel <samuelemil@hotmail.com>
2017-04-20 19:21:42 +0200

    Add files via upload

--- P3/pract3b.asm
+++ P3/pract3b.asm
@@ -1,42 +1,49 @@
-PRAC3A SEGMENT BYTE PUBLIC 'CODE'
-PUBLIC _encuentraSubcadena
-ASSUME CS: PRAC3A
-_encuentraSubcadena PROC FAR
+; DEFINICION DEL SEGMENTO DE DATOS
+DATOS SEGMENT
+VECTOR1 DB 1,2,4,8,5,10,9,7,3,6 ;; Vector de valores a sumar
+CONVERSION DB ?
+;-- rellenar con los datos solicitados
+DATOS ENDS
+
+PRAC3B SEGMENT BYTE PUBLIC 'CODE'
+PUBLIC _calculaSegundoDC
+ASSUME CS: PRAC3B
+_calculaSegundoDC PROC FAR
 	push bp
 	mov bp, sp
-	push bx si di cx
+	push ax bx dx si di
 	mov si, bp[6] ;; Cadena
-	mov di, bp[10] ;; Subcadena
-	mov cx, 0
-	mov ax, 1 ;; Por defecto son iguales
+	mov di, 0
 	
 continuar: 
-	mov bl, [si] ;; cadena
-	mov bh, [di] ;; Subadena
-	cmp bh, 0 ;; Subcadena[i] termina?
-	je final ;; Acaban ambas cadenas
-	cmp bl, 0
-	je fallo
-	cmp bl, bh ;; Comparamos el caracter
-	jne reubicamos
-
-	; Pasamos al siguiente carácter
+	mov bh, VECTOR1[di]
+	mov al, [si] ;; cadena
+	cmp al, 0 ;; fin de la cuenta
+	je modulo ;; obtenemos el modulo del numero obtenido respecto de 11
+	mul bh
+	add dx , ax 
 	inc si
 	inc di
-	jmp continuar	
-reubicamos:
-	inc cx
-	; Reiniciamos las cadenas
-	mov si, bp[6] 
-	mov di, bp[10]
-	add si, cx ;; Probamos desde el caracter i-esimo de la cadena
 	jmp continuar
-fallo:
-	mov ax, -1
+
+caso_especial:
+	mov ax, 9
+	jmp final
+	
+modulo:
+	cmp dx, 10
+	je caso_especial
+	mov ax, dx
+	div 11
+	mov ax, dx
+
 final: 
-	pop cx di si bx bp
+	pop di si dx bx ax
 	ret
-_encuentraSubcadena ENDP
-PRAC3A ENDS ; FIN DEL SEGMENTO DE CODIGO
+_calculaSegundoDC ENDP
+
+
+
+PRAC3B ENDS ; FIN DEL SEGMENTO DE CODIGO
 END ; FIN DE pract3a.asm
 	
